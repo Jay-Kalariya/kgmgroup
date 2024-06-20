ab043b8ff5952d4e4b5441ab53bf840b0a4bccd8
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-06-20 11:58:30 +0530

    dsd

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.module.css"
+function Header() {    
+    return(
+        <header>
+            <nav>
+                <div>
+                   <img src="" alt="" /> 
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+};
+export default Header;
