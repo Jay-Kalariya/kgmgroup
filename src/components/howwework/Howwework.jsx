7283bb570db8585601db428a7ffac651ebcd2dff
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-07-01 13:29:09 +0530

    jeel -m

---
+++ src/components/howwework/Howwework.jsx
@@ -0,0 +1,9 @@
+import styles from "./howwework.module.css"
+function Howwework() {    
+    return(
+        <div>
+            <p>how we work </p>
+        </div>
+    )
+};
+export default Howwework;
