d1885f8a7b4a3a3e88aac361c932449c26e4b20e
jay-kalariya <jaykalariya1209@gmail.com>
2024-07-02 00:15:38 +0530

    sdds

--- src/components/threeDModel/Model.jsx
+++ src/components/threeDModel/Model.jsx
@@ -1,9 +1,14 @@
+// "use server"
 import styles from "./Model.module.css"
+// import Spline from '@splinetool/react-spline';
+// import Spline from '@splinetool/react-spline/next';
 function Model() {    
     return(
           <div className={styles.Text}>
-            <p>3D Model</p>
+            <p>3d Model</p>
+            {/* <Spline scene="https://my.spline.design/untitled-c7fe10b86bd7f6743f8acd38b0abcd58/" /> */}
           </div>
     )
 };
+// https://my.spline.design/untitled-c7fe10b86bd7f6743f8acd38b0abcd58/
 export default Model;
