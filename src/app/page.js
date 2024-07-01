ae25c4824c9d3636dd6bc05e6bf6afd892f57495
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-07-01 15:10:36 +0530

    sdsd

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 // import BigText from "@/components/bigTextHeading/BigText";
 import Abc from "@/components/headingtextdemo/Textdemo";
-import Howwework from "@/components/howwework/howwework";
+import Howwework from "@/components/howwework/Howwework";
 import PortfolioCard from "@/components/portfolio/PortfolioCard";
 import Model from "@/components/threeDModel/Model";
 export default function Home() {
