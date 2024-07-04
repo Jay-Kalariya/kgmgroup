2004a0d0afbeb74b879c7b77d7d4452d284c826c
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-07-04 15:01:21 +0530

    DSDS

--- src/components/fourcards/FourCards.jsx
+++ src/components/fourcards/FourCards.jsx
@@ -1,10 +1,9 @@
 "use client"
 import { useEffect, useRef } from "react";
-// import React from 'react';
 import gsap from "gsap";
 import styles from './cards.module.css';
-// import AnimatedNumber from '../animatednumber/AnimatedNumber';
-// import PortfolioCardItem from '../portfolio/portfolioCardItem/PortfolioCardItem';
+import BigText from "../bigTextHeading/BigText";
+
 const AchievedBlock = ({xDirection, RotateDirection, numberOne, numberTwo, category, blockClass }) => {
   const imageRef = useRef(null);
   const cardRef = useRef(null);
@@ -49,8 +48,15 @@ const AchievedBlock = ({xDirection, RotateDirection, numberOne, numberTwo, categ
 };
 
 const FourCards = () => {
+  const texts = ['WHO',];
+  const texts1 = ['WE ARE'];
   return (
     <section className={styles.trSection}>
+       <div className={styles.BigTextHeadingInner}>
+      <BigText texts={texts} xDirection={"-40px"} X1Direction={"100px"} X2Direction={"100%"}/>
+      <BigText texts={texts1} xDirection={"200px"} X1Direction={"100px"} X2Direction={"100%"}/>
+
+    </div>
       <div className={styles.trHomeAchievedContainer}>
         <AchievedBlock xDirection={"-300"} RotateDirection={"-40"} numberOne="50" numberTwo="50" category="awards & recognition" blockClass={styles.trHomeAchievedBlock1} />
         <AchievedBlock xDirection={"300"} RotateDirection={"40"} numberOne="900" numberTwo="900" category="projects completed" blockClass={styles.trHomeAchievedBlock2} />
