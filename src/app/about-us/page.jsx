b78326e18e9ffef9717710d24ffdbb80121226fd
Jay-Kalariya <Jay-kalariya>
2024-07-21 17:49:23 +0530

    s

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,10 +1,13 @@
 import AboutUs from "@/components/aboutus/page";
 import Footer from "@/components/footer/footer";
+import styles from "@/app/about-us/aboutus.module.css";
 const AboutUsMain = () => {
     return (
         <>
+        <div className={styles.AboutUsContainer}>
         <AboutUs />
         <Footer />
+        </div>
         </>
     )
 }
