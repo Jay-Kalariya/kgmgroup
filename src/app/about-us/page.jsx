2634f701b72bffe2376995339c8c24868696a72e
Jay-Kalariya <Jay-kalariya>
2024-07-21 22:03:43 +0530

    sa

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,11 +1,14 @@
 import AboutUs from "@/components/aboutus/page";
 import Footer from "@/components/footer/footer";
 import styles from "@/app/about-us/aboutus.module.css";
+import Marquee_Sec from "@/components/marquee/page"
 const AboutUsMain = () => {
     return (
         <>
         <div className={styles.AboutUsContainer}>
         <AboutUs />
+        <Marquee_Sec direction="right"/>
+        <Marquee_Sec direction="left"/>
         <Footer />
         </div>
         </>
