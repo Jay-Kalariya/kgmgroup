c2660749827e4dbc68819763fec34dc19443db0f
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-07-24 15:58:55 +0530

    ds -j

--- src/components/marquee_final/page.jsx
+++ src/components/marquee_final/page.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Marquee from "react-fast-marquee";
 import "./marqueefinal.scss";
 import Image from 'next/image';
-import intertek from '@/images/intertek.png'
-import iso from '@/images/iso.png'
+import intertek from '@/ImagesNew/marquee1.svg';
+import iso from '@/ImagesNew/marquee2.svg'
+import Img1 from "@/ImagesNew/marquee3.svg";
+import Img2 from "@/ImagesNew/marquee4.svg";
 
 const Certification = () => {
   return (
@@ -26,16 +28,16 @@ const Certification = () => {
         </Marquee>
         <Marquee speed={50} loop={0} direction='left' pauseOnHover={true}> 
           <div className='certificate_marqee'>
-            <Image className='logocertificate' src={intertek} alt="certificationimg" />
-            <Image className='logocertificate' src={iso} alt="certificationimg" />
-            <Image className='logocertificate' src={intertek} alt="certificationimg" />
-            <Image className='logocertificate' src={iso} alt="certificationimg" />
-            <Image className='logocertificate' src={intertek} alt="certificationimg" />
-            <Image className='logocertificate' src={iso} alt="certificationimg" />
-            <Image className='logocertificate' src={intertek} alt="certificationimg" />
-            <Image className='logocertificate' src={iso} alt="certificationimg" />
-            <Image className='logocertificate' src={intertek} alt="certificationimg" />
-            <Image className='logocertificate' src={iso} alt="certificationimg" />
+            <Image className='logocertificate' src={Img1} alt="certificationimg" />
+            <Image className='logocertificate' src={Img2} alt="certificationimg" />
+            <Image className='logocertificate' src={Img1} alt="certificationimg" />
+            <Image className='logocertificate' src={Img2} alt="certificationimg" />
+            <Image className='logocertificate' src={Img1} alt="certificationimg" />
+            <Image className='logocertificate' src={Img2} alt="certificationimg" />
+            <Image className='logocertificate' src={Img1} alt="certificationimg" />
+            <Image className='logocertificate' src={Img2} alt="certificationimg" />
+            <Image className='logocertificate' src={Img1} alt="certificationimg" />
+            <Image className='logocertificate' src={Img2} alt="certificationimg" />
           </div>
         </Marquee>
       </div>
