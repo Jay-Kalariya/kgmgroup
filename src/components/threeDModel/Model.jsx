ab103c1463702dfb654472fda3e79c60fdb8471b
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-01 18:05:59 +0530

    df

--- src/components/threeDModel/Model.jsx
+++ src/components/threeDModel/Model.jsx
@@ -6,9 +6,10 @@ function Model() {
     return(
           <div className={styles.Text}>
             {/* <p>3d Model</p> */}
-            <Spline scene="https://prod.spline.design/xVRgGGckRGr91o9r/scene.splinecode" className={styles.Spliendesign}/>
+            <Spline scene="https://prod.spline.design/f022O34znqOzSb5J/scene.splinecode" className={styles.Spliendesign}/>
           </div>
     )
 };
 // https://my.spline.design/untitled-c7fe10b86bd7f6743f8acd38b0abcd58/
+// https://prod.spline.design/f022O34znqOzSb5J/scene.splinecode
 export default Model;
