e70b88106de0f80a819cb04c03da13273d6d696c
Ashish-jamnani <ashishjamnani001@gmail.com>
2024-08-05 18:34:34 +0530

    saas

--- src/components/threedfirstpage/page.jsx
+++ src/components/threedfirstpage/page.jsx
@@ -1,35 +1,71 @@
-// "use client"
-// import { useEffect, useRef, useState } from 'react';
-import styles from "./firstpage.module.css";
+// HomeBanner.js
+import { useEffect, useRef } from 'react';
+import styles from './firstpage.module.css';
 import Spline from '@splinetool/react-spline';
-// import Spline from "@splinetool/react-spline/next";
-const HomeBanner =({loadImage}) =>{
-  // const [splineLoaded, setSplineLoaded] = useState(false);
-  // const isMounted = useRef(false);
+import { TweenMax } from 'gsap';
 
-  // useEffect(() => {
-  //   if (isMounted.current) {
-  //     if (typeof loadImage === 'function') {
-  //       loadImage(!splineLoaded); // Notify parent that the component is fully loaded or not
-  //     } else {
-  //       console.error('loadImage prop is not a function');
-  //     }
-  //   } else {
-  //     isMounted.current = true;
-  //   }
-  // }, [splineLoaded, loadImage]);
+const HomeBanner = ({ loadImage }) => {
+  const bigBallRef = useRef(null);
+  const smallBallRef = useRef(null);
+  const hoverableRefs = useRef([]);
+
+  useEffect(() => {
+    const onMouseMove = (e) => {
+      TweenMax.to(bigBallRef.current, 0.4, {
+        x: e.pageX - 15,
+        y: e.pageY - 15,
+      });
+      TweenMax.to(smallBallRef.current, 0.1, {
+        x: e.pageX - 5,
+        y: e.pageY - 7,
+      });
+    };
+
+    const onMouseHover = () => {
+      TweenMax.to(bigBallRef.current, 0.3, { scale: 4 });
+    };
+
+    const onMouseHoverOut = () => {
+      TweenMax.to(bigBallRef.current, 0.3, { scale: 1 });
+    };
+
+    document.body.addEventListener('mousemove', onMouseMove);
+    hoverableRefs.current.forEach((element) => {
+      element.addEventListener('mouseenter', onMouseHover);
+      element.addEventListener('mouseleave', onMouseHoverOut);
+    });
+
+    return () => {
+      document.body.removeEventListener('mousemove', onMouseMove);
+      hoverableRefs.current.forEach((element) => {
+        element.removeEventListener('mouseenter', onMouseHover);
+        element.removeEventListener('mouseleave', onMouseHoverOut);
+      });
+    };
+  }, []);
 
   return (
     <div className={styles.Text}>
+      <div className={styles.cursor}>
+        <div ref={bigBallRef} className={`${styles.cursor__ball} ${styles.cursor__ballbig}`}>
+          <svg height="30" width="30">
+            <circle cx="15" cy="15" r="12" strokeWidth="0"></circle>
+          </svg>
+        </div>
+        <div ref={smallBallRef} className={`${styles.cursor__ball} ${styles.cursor__ballsmall}`}>
+          <svg height="10" width="10">
+            <circle cx="5" cy="5" r="4" strokeWidth="0"></circle>
+          </svg>
+        </div>
+      </div>
       <div className={styles.TextInnerModel}>
         <p className={styles.InnerTextModule}>
           Roar Data <br /> Bridge the Gap Between Data and Decision-Making
-        </p>  
+        </p>
       </div>
       <Spline
         scene="https://prod.spline.design/Qr4-g4dqy2Kpwg1O/scene.splinecode"
         className={styles.Spliendesign}
-        // onLoad={() => setSplineLoaded(true)}
       />
       <div className={styles.InnerTextfield}>
         <p className={styles.InnerTextfieldOne}>
@@ -41,11 +77,12 @@ const HomeBanner =({loadImage}) =>{
         </p>
       </div>
       <div className={styles.InnerTextfieldScroll}>
-      <p className={`${styles.scrollDown} ${styles.otherClass}`}>scroll down</p>
+        <p 
+         ref={(el) => el && hoverableRefs.current.push(el)}
+        className={styles.scrollDown}>scroll down</p>
       </div>
     </div>
-    
   );
-}
-// https://prod.spline.design/Qr4-g4dqy2Kpwg1O/scene.splinecode
+};
+
 export default HomeBanner;
